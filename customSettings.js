89d699515dde6ca3638c95efec7492ad1f99dc85
MainCharacterofTheProblem <81876472+MainCharacterofTheProblem@users.noreply.github.com>
2022-06-04 19:56:06 -0400

    actually, ignore the last dozen commits

--- customSettings.js
+++
@@ -1,6 +0,0 @@
-let customAlgorithmModulatorSetting1 = 1;
-let customAlgorithmModulatorSetting2 = 2;
-let customAlgorithmModulatorSetting3 = 3; 
-let customAlgorithmModulatorSetting4 = 4;
-let customAlgorithmModulatorSetting5 = 5;
-let customAlgorithmModulatorSetting6 = 6;
