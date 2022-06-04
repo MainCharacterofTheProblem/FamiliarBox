d039e515c8f2deccc5be9177ee903db4faa51721
MainCharacterofTheProblem <81876472+MainCharacterofTheProblem@users.noreply.github.com>
2022-06-04 19:39:33 -0400

    TEMP FILE, WILL CHANGE LATER

--- customSettings.js
+++ customSettings.js
@@ -1,6 +1,6 @@
 let customAlgorithmModulatorSetting1 = 1;
 let customAlgorithmModulatorSetting2 = 2;
-let customAlgorithmModulatorSetting3 = 3;
+let customAlgorithmModulatorSetting3 = 3; 
 let customAlgorithmModulatorSetting4 = 4;
 let customAlgorithmModulatorSetting5 = 5;
 let customAlgorithmModulatorSetting6 = 6;
